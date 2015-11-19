ec492e01b8eed8426e584a3b57b0b3390844d309
Nathan Levine <natejlevine@gmail.com>
2015-11-19 15:28:35 -0800

    scaffolded out files/folders

--- db/config.js
+++ db/config.js
@@ -5,7 +5,9 @@ var mongoose = require('mongoose');
 
 // Currently configured for deployment. Change to this for development:
 // ```mongoose.connect('mongodb://127.0.0.1');```
-mongoose.connect(process.env.MONGOLAB_URI);
+// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect('mongodb://127.0.0.1/whiteboard');
+
 
 var db = mongoose.connection;
 
