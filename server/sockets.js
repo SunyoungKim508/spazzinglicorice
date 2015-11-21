0949ab650f8305238a33112a0f877eb03629113c
SunyoungKim508 <sunyoungkim508@gmail.com>
2015-11-20 23:02:04 -0800

    closes #24

--- server/sockets.js
+++ server/sockets.js
@@ -4,10 +4,7 @@
 
 // Import board model from [board.js](../documentation/board.html)
 var Board = require('../db/board');
-// var EventEmitter = require('events').EventEmitter;
 
-// var emitter = new EventEmitter();
-// emitter.setMaxListeners(0);
 // **boardUrl:** *String* <br>
 // **board:** *Mongoose board model* <br>
 // **io:** *Export of our Socket.io connection from [server.js](../documentation/server.html)*
@@ -18,13 +15,12 @@ var connect = function(boardUrl, board, io) {
 
   whiteboard.once('connection', function(socket) {
     //require our separate modules - drawing, chat, etc...
-    console.log('work');
-    require('./drawing/drawing.js')(socket, Board);
+
     socket.on('chat message', function (msg) {
-      console.log('are you working?')
-      console.log('chatter' + msg);
+      console.log('chatter: ' + msg);
       whiteboard.emit('chat message', msg);
     });
+    require('./drawing/drawing.js')(socket, Board);
     // require('./chatter/chatter.js')(socket, whiteboard);
 
     // Send the current state of the board to the client immediately on joining.
