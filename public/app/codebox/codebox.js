adcdd966f30660d227e117e1116ed6a864ec2787
Ying Hao Chan <yinghao@outlook.com>
2015-11-21 15:08:31 -0800

    merge rebase

--- public/app/codebox/codebox.js
+++ public/app/codebox/codebox.js
@@ -1,5 +1,44 @@
-angular.module('devslate.codebox', [])
+angular.module('devslate.codebox', ['ui.codemirror'])
 
-.controller('CodeboxCtrl', function ($scope) {
+.controller('CodeboxCtrl', function (Socket, $scope) {
+  var api = Socket.socket;
+   $scope.editorOptions = {
+        lineWrapping : true,
+        lineNumbers: true,
+        mode: 'javascript',
+        value: 'asdasdas',
+        theme: 'solarized light',
+        tabSize: 2,
+        keyMap: 'sublime'
+    };
+
+
+
+  $scope.send = function () {
+    api.emit('code-text', $scope.code);
+  };
+
+  
+
+  $scope.codebox = function (_editor) {
+     // Editor part
+      var _doc = _editor.getDoc();
+      _editor.focus();
+
+      // Options
+      // _editor.setOption('firstLineNumber', 10);
+      _doc.markClean();
+
+      var copy = _doc.linkedDoc();
+
+      api.on('code-text', function (data) {
+        copy.setValue(data);
+         });  
+
+     //  // Events
+     //  _editor.on("beforeChange", function(){ ... });
+     //  _editor.on("change", function(){ ... });
+    };
+  
 
 });
--- server/sockets.js
+++ server/sockets.js
@@ -4,7 +4,10 @@
 
 // Import board model from [board.js](../documentation/board.html)
 var Board = require('../db/board');
+// var EventEmitter = require('events').EventEmitter;
 
+// var emitter = new EventEmitter();
+// emitter.setMaxListeners(0);
 // **boardUrl:** *String* <br>
 // **board:** *Mongoose board model* <br>
 // **io:** *Export of our Socket.io connection from [server.js](../documentation/server.html)*
@@ -12,12 +15,17 @@ var connect = function(boardUrl, board, io) {
 
   // Set the Socket.io namespace to the boardUrl.
   var whiteboard = io.of(boardUrl);
-  console.log('hey');
+
   whiteboard.once('connection', function(socket) {
     //require our separate modules - drawing, chat, etc...
-    console.log('are you working whiteboard?');
+    console.log('work');
     require('./drawing/drawing.js')(socket, Board);
-    require('./chatter/chatter.js')(socket, whiteboard);
+    socket.on('chat message', function (msg) {
+      console.log('are you working?')
+      console.log('chatter' + msg);
+      whiteboard.emit('chat message', msg);
+    });
+    // require('./chatter/chatter.js')(socket, whiteboard);
 
     // Send the current state of the board to the client immediately on joining.
     socket.emit('join', board);
