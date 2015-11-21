b169af3b09083b208b7afa4461df8362c8e14ef1
Nathan Levine <natejlevine@gmail.com>
2015-11-21 11:14:51 -0800

    merged sun's frontend changes over manually (chatbox/user services/ctrl/html)

--- public/app/chatbox/chatbox.js
+++ public/app/chatbox/chatbox.js
@@ -1,5 +1,27 @@
 angular.module('devslate.chatbox', [])
 
-.controller('ChatboxCtrl', function ($scope) {
+.controller('ChatboxCtrl', function ($scope, Socket) {
+  // var ioRoom = window.location.href;
+  // all messages from users will be stored in texts array
+  $scope.texts = [];
+  // scope.watch
 
+  // using io instance(socket) by calling it ioRoom(namespace)
+  // var socket = io(ioRoom);
+  $scope.sendMessage = function (message, $event) {
+    $event.preventDefault();
+    console.log('angular: '+ message);
+    Socket.socket.emit('chat message', message);
+    $scope.message = '';
+    return false;
+  };
+
+  Socket.socket.on('chat message', function (msg) {
+    console.log('socket'+ msg);
+    $scope.texts.push(msg);
+  });
+
+  $scope.$watch('texts', function () {
+    console.log('hi: texts');
+  });
 });
--- public/app/user/user.js
+++ public/app/user/user.js
@@ -1,5 +1,50 @@
 angular.module('devslate.user', [])
 
-.controller('UserCtrl', function ($scope) {
+.controller('UserCtrl', function ($scope, $location, User, Board) {
+  $scope.createBoard = function (name) {
+    console.log('createBoard');
+    Board.createBoard(name)
+      .then(function (res) {
+        console.log(res);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    // $location.path('/new');
+  };
 
+  console.log('user', User);
+  $scope.getTodos = function () {
+    User.getTodos()
+      .then(function (todos) {
+        $scope.todos = todos;
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
+
+  $scope.getBookmarks = function () {
+    User.getBookmarks()
+      .then(function (bookmarks) {
+        $scope.bookmarks = bookmarks;
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
+
+  $scope.getTodos();
+  $scope.getBookmarks();
+
+  $scope.getBookmark = function (url) {
+    User.getBookmark(url)
+      .then(function (res) {
+        console.log(res);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    // $location.path('/'+url);
+  };
 });
