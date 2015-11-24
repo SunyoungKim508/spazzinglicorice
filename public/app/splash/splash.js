8317a38d48c9543d544b90fbe63e87f9b55d99cc
Lucilla Chalmer <chalmer.lucilla@gmail.com>
2015-11-24 12:42:01 -0800

    Updated splash logic to show different views based on log-in status

--- public/app/splash/splash.js
+++ public/app/splash/splash.js
@@ -1,5 +1,8 @@
 angular.module('devslate.splash', [])
 
-.controller('SplashCtrl', function ($scope, $stateParams) {
-  $scope.userIsAuth = false;
+.controller('SplashCtrl', function ($scope, $stateParams, Authenticate, $http) {
+  $http.get('/authenticate').then(function (response) {
+    var facebookId =  response.data;
+    $scope.userIsAuth = facebookId;
+  });
 });
