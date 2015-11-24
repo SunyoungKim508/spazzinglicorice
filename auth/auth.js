541cafa89ff7b896cb2a9be318ef6e4116f8760d
Lucilla Chalmer <chalmer.lucilla@gmail.com>
2015-11-24 13:57:55 -0800

    Modified front end auth to use promisses. Logic for auth is now in isolated to Authentication factory. Should create local variable to store log-in status to min http requests on page change

--- auth/auth.js
+++ auth/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
   'facebookAuth' : {
-    'clientID'      : '1637653006488678', // your App ID
+    'clientID'      : '1637653006488678', // App ID
     'clientSecret'  : '18d6b88b531d08138ace0891441e097d', // your App Secret
     // 'callbackURL'   : 'http://devslate.elasticbeanstalk.com/auth/facebook/callback'
     'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
