12e4235b9d0430f78a97e0e501b5420de3b58aae
Lucilla Chalmer <chalmer.lucilla@gmail.com>
2015-11-29 15:56:27 -0800

    adding user prof pic -- not working yet

--- public/app/user/user.js
+++ public/app/user/user.js
@@ -1,13 +1,23 @@
 angular.module('devslate.user', [])
 
-.controller('UserCtrl', function ($scope, $location, User, BoardDB) {
+.controller('UserCtrl', function ($scope, $location, User, BoardDB, $http) {
 // Set toolbar for colour palette and eraser. 
   $scope.todos = [];
   $scope.bookmarks = [];
 
+  $scope.getUser = function (url) {
+    // send get request with facebook ID to get user profile picture
+    $http.get('graph.facebook.com/v2.5/10208138228752958/picture').then(function (data) {
+      console.log('data inside get user: ', data);
+    })
+    $scope.profPicUrl = res;
+  };
+  console.log("running get user");
+  $scope.getUser();
+
   $scope.bookmark = function (url) {
     $location.path(url);
-  }
+  };
 
   // TODO
   $scope.createBoard = function (name) {
