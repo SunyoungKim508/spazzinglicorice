b0d001d10cbd76bf9b1f2f32d592092d1ea405d8
Ying Hao Chan <yinghao@outlook.com>
2015-11-29 21:17:03 -0800

    attempted adding ot.js

--- public/app/codebox/codebox.js
+++ public/app/codebox/codebox.js
@@ -1,44 +1,57 @@
 angular.module('devslate.codebox', ['ui.codemirror'])
 
-.controller('CodeboxCtrl', function (Socket, $scope) {
-  var api = Socket;
-   $scope.editorOptions = {
-        lineWrapping : true,
-        lineNumbers: true,
-        mode: 'javascript',
-        value: 'asdasdas',
-        theme: 'solarized light',
-        tabSize: 2,
-        keyMap: 'sublime'
-    };
+.controller('CodeboxCtrl', function(Socket, $scope) {
+  // set socket.io instance var
+  var ioApi = Socket;
 
-
-
-  $scope.send = function () {
-    Socket.emit('code-text', $scope.code);
+  // options for loading codemirror
+  $scope.editorOptions = {
+    lineWrapping: true,
+    lineNumbers: true,
+    mode: 'javascript',
+    value: 'asdasdas',
+    theme: 'solarized light',
+    tabSize: 2,
+    keyMap: 'sublime'
   };
 
-
-
-  $scope.codebox = function (_editor) {
-     // Editor part
-      var _doc = _editor.getDoc();
-      _editor.focus();
-
-      // Options
-      // _editor.setOption('firstLineNumber', 10);
-      _doc.markClean();
-
-      var copy = _doc.linkedDoc();
-
-      api.on('code-text', function (data) {
-        copy.setValue(data);
-         });
-
-     //  // Events
-     //  _editor.on("beforeChange", function(){ ... });
-     //  _editor.on("change", function(){ ... });
+  /**
+   * codebox instance
+   * @param   {obj}  _cm  cm instance
+   */
+  $scope.codebox = function(_cm) {
+    var cmClient;
+    
+    /**
+     * initializes operational transform library
+     * @param   {str}  str            the initial text for doc
+     * @param   {??}  revision       revision number to track updates
+     * @param   {??}  clients        [description]
+     * @param   {[type]}  serverAdapter  [description]
+     * @return  {[type]}                 [description]
+     */
+    var init = function(str, revision, clients, serverAdapter) {
+      _cm.setValue(str);
+      cmClient = new ot.EditorClient(revision, clients, serverAdapter, new ot.CodeMirrorAdapter(cm));
     };
 
+    //define cm doc
+    var _doc = _cm.getDoc();
+    _cm.focus();
+
+    // Options
+    // _cm.setOption('firstLineNumber', 10);
+    _doc.markClean();
+
+    var copy = _doc.linkedDoc();
+
+  };
+
+  /**
+   * initialize cm text with ot on socket recieving doc
+   */
+  ioApi.on('doc', function(obj) {
+    init(obj.str, obj.revision, obj.clients, new ot.SocketIOAdapter(ioApi));
+  });
 
 });
