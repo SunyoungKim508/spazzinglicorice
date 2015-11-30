1622480df514d9fd5f75c9e36d4bdb7b0e6cf5b7
Lucilla Chalmer <chalmer.lucilla@gmail.com>
2015-11-29 16:35:19 -0800

    Added facebook profile pic to users page. FB ID is hard coded to my ID, need to modify to be user specific

--- public/app/user/user.js
+++ public/app/user/user.js
@@ -1,16 +1,20 @@
 angular.module('devslate.user', [])
 
-.controller('UserCtrl', function ($scope, $location, User, BoardDB, $http) {
+.controller('UserCtrl', function ($scope, $location, User, $http) {
 // Set toolbar for colour palette and eraser. 
   $scope.todos = [];
   $scope.bookmarks = [];
+  $scope.profPicUrl = 'http://s3.amazonaws.com/readers/2011/10/29/penguin--openclipart--by-maw_2.png';
 
   $scope.getUser = function (url) {
     // send get request with facebook ID to get user profile picture
-    $http.get('graph.facebook.com/v2.5/10208138228752958/picture').then(function (data) {
+    $http({
+      method: 'GET',
+      url:'https://graph.facebook.com/10208138228752958/picture?redirect=false&type=large'
+    }).then(function (data) {
       console.log('data inside get user: ', data);
+      $scope.profPicUrl = data.data.data.url;
     })
-    $scope.profPicUrl = res;
   };
   console.log("running get user");
   $scope.getUser();
